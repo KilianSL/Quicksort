3e6e95fbce55e874e5ed038bcb9dbba17a362665
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-04-01 12:01:39 +0100

    Added basic recursive quicksort

---
+++ BasicRecursiveQuicksort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class BasicRecursiveQuicksort : sorter
+    {
+        public BasicRecursiveQuicksort() : base("quicksort") { }
+        public override void sort(int[] tosort)
+        {
+            sortRecursive(ref tosort, 0, tosort.Length - 1);
+            Console.WriteLine($"{this.name} properly sorted: {verifySorted(tosort)}");
+        }
+
+        private void sortRecursive(ref int[] arr, int lo, int hi) //lo = start index, hi = end index for partition. Using indexes like this allows the algorithm to work in-place
+        { // This method controls the recursion and flow of the sort  
+            if (lo < hi)
+            {
+                // pi is the partition index i.e. the point on which the array pivots
+                int pi = partition(ref arr, lo, hi); //Passes array by reference: less memory overhead
+
+                sortRecursive(ref arr, lo, pi - 1); // Sorts lower sub-list
+                sortRecursive(ref arr, pi + 1, hi); // Sorts higher sub-list
+
+            }
+        }
+
+        private int partition(ref int[] arr, int lo, int hi)
+        { //This method performs the actual sort - it pivots around a certain point and ensures all values within range lo and hi sit at the right side of the point
+
+            int pi = arr[hi]; //Gets element to pivot on
+            int i = (lo - 1);
+
+            for (int j = lo; j < hi; j++)
+            {
+                if (arr[j] < pi) // Comparison -> is the selected element lower than the pivot?
+                {
+                    i++; // Increment index of smallest element
+                    int _x = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = _x; // Swaps the element in the wrong place with the smallest element
+                }
+            }
+            int _ = arr[i + 1];
+            arr[i + 1] = arr[hi];
+            arr[hi] = _;
+
+            return (i + 1);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickSort
+{
+    abstract class sorter
+    {
+        protected String name { get; }
+        public String GetName() { return name; }
+        public sorter(String name) { this.name = name; }
+        public abstract void sort(int[] tosort);
+        protected static bool verifySorted(int[] arr) // Helper function to verify the sorting algo worked properly. 
+        {
+            var sorted = new int[arr.Length];
+            arr.CopyTo(sorted, 0); // Creates copy of arr to compare to
+
+            Array.Sort(sorted);
+
+            return sorted.SequenceEqual(arr); // Checks implemented sort is equal to properly sorted array 
+        }
+
+    }
+
+    class Program
+    {
+        
+
+        
+
+       
+
+
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] tosort = new int[10000];
+            for (int i = 0; i < tosort.Length; i++)
+            {
+                tosort[i] = rand.Next(0, 50000); //populates an array of 10000 random ints
+            }
+
+            var timer = new System.Diagnostics.Stopwatch();
+            var sorters = new System.Collections.Generic.List<sorter>();
+            sorters.Add(new quicksort());
+
+            foreach (var s in sorters)
+            {
+                timer.Reset();
+                timer.Start();
+                s.sort(tosort);
+
+                Console.WriteLine("{1} sort, Elapsed={0}", timer.Elapsed, s.GetName());
+            }
+        }
+    }
+}
